dc6f67f30e10b6fcbd1fd80d88580313fb59d565
ahsankhan609 <ahsankhan609@gmail.com>
2024-04-30 13:11:16 +0500

    make some fresh changes

--- project.py
+++ project.py
@@ -1,24 +1,29 @@
 print(
-    "Welcome to Employee Salary Portal. Here will show you how much salary you owe to the Compnay. Please provide some info to calculate your salary."
+    """Welcome to Employee Salary Portal.
+     Here will show you how much salary you owe to the Company.
+      Please provide some info to calculate your salary."""
 )
 
-your_name = input("\nWhat is your Name? ") #taking name input from user.
-#converting hourly rate to float data type
+# taking name input from user. And converting hourly rate to float data type
+
+your_name = input("\nWhat is your Name? ")
 hourly_wage_rate = float(input("what is your Hourly Wage Rate? "))
-hours_wrorked_this_week = int(input("Hours hours_wrorked_this_week? "))
+hours_worked_this_week = int(input("Hours hours_worked_this_week? "))
 
-#checking first condition:
-if hours_wrorked_this_week > 40:
-        wage_without_ot = hourly_wage_rate * 40
-        ot = (hourly_wage_rate * 1.10) * (hours_wrorked_this_week - 40)
-        total_wage = wage_without_ot + ot
-        print(f"{your_name}! Your Total Wgae is {total_wage}. Gross Wage is {wage_without_ot} and Overtime is {ot}.")
+# checking first condition:
 
-#checking secondt condition:
-elif hours_wrorked_this_week == 40:
-        print(f"{your_name}! Your total wage is {hourly_wage_rate * hours_wrorked_this_week}.")
+if hours_worked_this_week > 40:
+    wage_without_ot = hourly_wage_rate * 40
+    ot = (hourly_wage_rate * 1.10) * (hours_worked_this_week - 40)
+    total_wage = wage_without_ot + ot
+    print(f"{your_name}! Your Total Wgae is {total_wage}. Gross Wage is {wage_without_ot} and Overtime is {ot}.")
 
-#writing default condition:
+# checking secondt condition:
+
+elif hours_worked_this_week == 40:
+    print(f"{your_name}! Your total wage is {hourly_wage_rate * hours_worked_this_week}.")
+
+# writing default condition:
+
 else:
-  print(f"{your_name}! You didn't work this Week.")
-
+    print(f"{your_name}! You didn't work this Week.")
